ce191a61fd65160866a116b3a28b10da664a3d41
natalie Lai <natalielai0109@gmail.com>
2022-12-24 14:03:03 +0800

    modify

--- main.py
+++ main.py
@@ -4,4 +4,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
+    print(f"request的attribute method:{request.method}")
+    print(f"request的attribute method:{request.mimetype_params}")
     return render_template('index.html')
