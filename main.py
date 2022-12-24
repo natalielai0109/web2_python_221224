7dbdee390ad8caf4f9ee3b90ca6039b4591e6fc4
natalie Lai <natalielai0109@gmail.com>
2022-12-24 16:57:01 +0800

    modify

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request,openpyxl
+from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
@@ -6,14 +6,21 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route("/login",methods = ["GET","POST"])
+from openpyxl import load_workbook
+@app.route('/login',methods=['GET','POST'])
 def login():
-    if request.method == 'POST':
+    if request.method == "POST":
         print('由表單傳送過來的')
         print(f"email:{request.form['email']}")
         print(f"password:{request.form['password']}")
+        wb = load_workbook('static/other/login.xlsx')
+        login_sheet = wb['工作表1']
+        for row in login_sheet.iter_rows():
+            print(row[0].value)
+            print(row[1].value)
     return render_template('login.html')
 
 @app.route("/name/<username>")
 def show_user(username):
     return f"<h3>您的姓名是<strong>{username}</strong></h3>"
+
